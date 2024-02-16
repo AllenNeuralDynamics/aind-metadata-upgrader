48806176212b040811fcab241a9fd8bd5d7a2f1e
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-02-16 12:45:17 -0600

    Update test_procedures.py

--- tests/test_procedures.py
+++ tests/test_procedures.py
@@ -7,15 +7,22 @@ from aind_metadata_upgrader.procedures_upgrade import ProcedureUpgrade
 
 from glob import glob
 import json
+from pathlib import Path
 
 procedures_files = glob("tests/resources/procedures/class_model_examples/*.json")
+print(procedures_files)
 
-with open(procedures_files[0]) as f:
-    print(procedures_files[0])
-    procedures = json.load(f)
-    print("input procedure: ", procedures)
-    ProcedureUpgrader = ProcedureUpgrade(procedures)
+for file in procedures_files:
+    with open(file) as f:
+        subject = Path(file).stem
+        print(file)
+        print(subject)
+        procedures = json.load(f)
+        print("input procedure: ", procedures)
+        ProcedureUpgrader = ProcedureUpgrade(procedures)
 
-    test = ProcedureUpgrader.upgrade_procedure()
+        test = ProcedureUpgrader.upgrade_procedure()
 
-    print(test)
+        test.write_standard_file(output_directory=Path("tests/resources/procedures/updated_class_models"), prefix=Path(subject))
+
+    break
