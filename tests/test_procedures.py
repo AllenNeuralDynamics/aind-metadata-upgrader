01f5f69ce4e1711d9fb3c1f6eb420673704c3755
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-02-20 18:33:32 -0500

    added logger

--- tests/test_procedures.py
+++ tests/test_procedures.py
@@ -8,9 +8,31 @@ from aind_metadata_upgrader.procedures_upgrade import ProcedureUpgrade
 from glob import glob
 import json
 from pathlib import Path
+import logging
+
+import logging
+from datetime import datetime
+# You can set the ouput location to whatever. You may need to create a 'logs' folder in the scratch
+# directory beforehand.
+
+log_file_name = "./tests/resources/procedures/log_files/log_" + datetime.now().strftime("%Y%m%d_%H%M%S") + ".log"
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler(log_file_name)
+fh.setLevel(logging.DEBUG)
+
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+# add the handlers to logger
+logger.addHandler(fh)
+
 
 procedures_files = glob("tests/resources/procedures/class_model_examples/*.json")
 print(procedures_files)
+
 
 for file in procedures_files:
     with open(file) as f:
@@ -25,4 +47,3 @@ for file in procedures_files:
 
         test.write_standard_file(output_directory=Path("tests/resources/procedures/updated_class_models"), prefix=Path(subject))
 
-    break
