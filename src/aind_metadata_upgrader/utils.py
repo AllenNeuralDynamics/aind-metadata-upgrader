cffdb81066798bf6ba808a4122ed886b3698250f
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-02-28 12:20:03 -0500

    expand get_or_default

--- src/aind_metadata_upgrader/utils.py
+++ src/aind_metadata_upgrader/utils.py
@@ -1,6 +1,8 @@
 from aind_data_schema.base import AindModel
 from pydantic.fields import PydanticUndefined
 
+import logging
+
 from aind_data_schema.core.procedures import (
     Procedures, 
     Surgery,
@@ -27,14 +29,15 @@ def check_field(model, field):
         return getattr(model, field)
     return None
 
-def get_or_default(model: AindModel, field_name: str, kwargs: dict):
+def get_or_default(model: dict, model_type: AindModel, field_name: str, kwargs: dict = {}):
     if kwargs.get(field_name) is not None:
         return kwargs.get(field_name)
-    elif hasattr(model, field_name) and getattr(model, field_name) is not None:
-        return getattr(model, field_name)
+    elif model.get(field_name, None) is not None:
+        logging.info(f"field_name: {field_name}, model.get(field_name): {model.get(field_name)}")
+        return model.get(field_name)
     else:
         try:
-            attr_default = getattr(type(model).model_fields.get(field_name), "default")
+            attr_default = getattr(model_type.model_fields.get(field_name), "default")
             if attr_default == PydanticUndefined:
                 return None
             return attr_default
