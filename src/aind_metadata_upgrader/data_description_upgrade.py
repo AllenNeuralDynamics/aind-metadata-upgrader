9835f62451e22540ff14786e971a481c5cb55248
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-06-17 17:07:38 -0400

    fixing isoformat error

--- src/aind_metadata_upgrader/data_description_upgrade.py
+++ src/aind_metadata_upgrader/data_description_upgrade.py
@@ -209,7 +209,7 @@ class DataDescriptionUpgrade(BaseModelUpgrade):
         creation_time = self._get_or_default(self.old_model, "creation_time", kwargs)
         old_name = self._get_or_default(self.old_model, "name", kwargs)
         if creation_date is not None and creation_time is not None:
-            creation_time = datetime.fromisoformat(f"{creation_date}T{creation_time}")
+            creation_time = datetime.fromisoformat(f"{creation_date}T{creation_time.replace('Z', "+00:00")}")
         elif creation_time is not None:
             creation_time = datetime.fromisoformat(f"{creation_time}")
         elif old_name is not None:
