7613453fa7ac72d84bb31db60b3133a4bd508dc1
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-08-29 12:41:33 -0400

    Update data_description_upgrade.py

--- src/aind_metadata_upgrader/data_description_upgrade.py
+++ src/aind_metadata_upgrader/data_description_upgrade.py
@@ -38,11 +38,7 @@ class ModalityUpgrade:
         "mesospim": Modality.SPIM,
         "single-plane-ophys": Modality.POPHYS,
         "multiplane-ophys": Modality.POPHYS,
-        "hsfp": Modality.FIB,
-        "fip": Modality.FIB,
         "trained-behaviors": Modality.BEHAVIOR,
-        "diSPIM": Modality.SPIM,
-        "multiplane-ophys": Modality.POPHYS,
     }
 
     @classmethod
