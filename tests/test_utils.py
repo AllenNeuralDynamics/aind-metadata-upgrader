b72edb790ee1d311c58b319213d96d4ed942d5a1
Mae Moninghoff <mae.moninghoff@alleninstitute.org>
2024-09-10 12:17:52 -0700

    cleaning up paths

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,12 +9,6 @@ from pydantic import __version__ as pyd_version
 
 from aind_metadata_upgrader.utils import get_or_default
 
-PYD_VERSION = re.match(r"(\d+.\d+).\d+", pyd_version).group(1)
-
-PROCESSING_FILES_PATH = Path(__file__).parent / "resources" / "ephys_processing"
-PYD_VERSION = re.match(r"(\d+.\d+).\d+", pyd_version).group(1)
-
-
 class TestUtils(unittest.TestCase):
     """Class for testing utilities."""
 
