bb6717188c3a2b0c1a06e4b3e65031e337973d38
Dan Birman <danbirman@gmail.com>
2025-06-13 21:56:47 -0700

    refactor: new QC format

--- src/aind_metadata_upgrader/quality_control/v1v2.py
+++ src/aind_metadata_upgrader/quality_control/v1v2.py
@@ -1,8 +1,51 @@
 """<=v1.4 to v2.0 quality control upgrade functions"""
 
-from aind_data_schema.core.quality_control import CurationMetric
-
 from aind_metadata_upgrader.base import CoreUpgrader
+from aind_data_schema.core.quality_control import QCMetric, CurationMetric
+
+
+def upgrade_metric(data: dict, modality: dict, stage: str, tags: list) -> dict:
+    """Upgrade a metric to the new format"""
+    if not isinstance(data, dict):
+        raise ValueError("Data must be a dictionary")
+
+    metric = QCMetric(
+        name=data.get("name", "unknown"),
+        modality=modality,
+        stage=stage,
+        value=data.get("value", None),
+        status_history=data.get("status_history", []),
+        description=data.get("description", None),
+        reference=data.get("reference", None),
+        tags=tags,
+        evaluated_assets=data.get("evaluated_assets", []),
+    )
+
+    return metric.model_dump()
+
+
+def upgrade_curation_metric(data: dict, modality: dict, stage: str, tags: list) -> dict:
+    """Upgrade a curation metric to the new format"""
+    if not type == "curation":
+        raise ValueError(f"Expected type 'curation', got {data.get('type', 'unknown')}")
+
+    curations = data["value"]["curations"]
+    curation_history = data["value"].get("curation_history", [])
+
+    metric = CurationMetric(
+        name=data.get("name", "unknown"),
+        modality=modality,
+        stage=stage,
+        tags=tags,
+        value=curations,
+        description=data.get("description", None),
+        reference=data.get("reference", None),
+        evaluated_assets=data.get("evaluated_assets", []),
+        type="unknown",
+        curation_history=curation_history,
+    )
+    
+    return metric.model_dump()
 
 
 class QCUpgraderV1V2(CoreUpgrader):
@@ -16,38 +59,32 @@ class QCUpgraderV1V2(CoreUpgrader):
 
         data["schema_version"] = schema_version
 
+        metrics = []
+
         # Add "object_type" to all the evaluations and metrics
         for ei, evaluation in enumerate(data.get("evaluations", [])):
-            if "object_type" not in evaluation:
-                data["evaluations"][ei]["object_type"] = "QC evaluation"
 
-            for mi, metric in enumerate(evaluation.get("metrics", [])):
-                if "object_type" not in metric:
-                    data["evaluations"][ei]["metrics"][mi]["object_type"] = "QC metric"
+            modality = evaluation.get("modality", {})
+            stage = evaluation.get("stage", "unknown")
+            tags = [evaluation["name"]]
 
-        # Handle upgrades to CurationMetric objects
-        for ei, evaluation in enumerate(data.get("evaluations", [])):
-
-            for mi, metric in enumerate(evaluation.get("metrics", [])):
-                if (
-                    isinstance(metric["value"], dict)
-                    and "type" in metric["value"]
-                    and metric["value"]["type"] == "curation"
-                ):
-                    # Bump the CurationMetric object up to the metric level
-                    curations = metric["value"]["curations"]
-
-                    new_metric = CurationMetric(
-                        name=metric.get("name", "unknown"),
-                        value=curations,
-                        description=metric.get("description", None),
-                        reference=metric.get("reference", None),
-                        evaluated_assets=metric.get("evaluated_assets", []),
-                        type="unknown",
-                        curation_history=metric.get("curation_history", []),
+            for metric in evaluation.get("metrics", []):
+                if "type" in metric:
+                    # CurationMetric, we'll deal with this in a second
+                    metric_data = upgrade_curation_metric(
+                        data=metric,
+                        modality=modality,
+                        stage=stage,
+                        tags=tags,
                     )
-
-                    data["evaluations"][ei]["metrics"][mi] = new_metric.model_dump()
+                else:
+                    metric_data = upgrade_metric(
+                        data=metric,
+                        modality=modality,
+                        stage=stage,
+                        tags=tags,
+                    )
+                metrics.append(metric_data)
 
         return {
             "object_type": "Quality control",
